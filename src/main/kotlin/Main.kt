091d4c9b0be873ef2a2653d03acc10e3d49b7d79
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-12 23:40:24 +0900

    Test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,5 @@ package org.example
 
 fun main() {
     println("Hello World!")
+    val user = User("yeseojng0412@dakds")
 }
---
+++ src/main/kotlin/User.kt
@@ -0,0 +1,5 @@
+package org.example
+
+interface User {
+    val nickName: String
+}
